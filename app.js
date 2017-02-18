afc50f99e2dc780a7e1272b1ab62e962e6223b79
Swarit Agarwal <agarwalswarit@gmail.com>
2017-02-18 21:08:15 +0800

    try to fix cors issue

--- app.js
+++ app.js
@@ -98,6 +98,17 @@ var sess;
     }
 });*/
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+	if (req.method === 'OPTIONS') {
+		res.end();
+	} else {
+		next();
+	}
+});
+
 app.get('/', function(req, res) {
     res.send('Hello! Ionic Node Server');
 });
